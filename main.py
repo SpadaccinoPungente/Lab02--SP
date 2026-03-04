e98014fd0b7d4d1f760c6ba505d168621860a0bd
Zuzze-spec <s268590@studenti.polito.it>
2026-03-04 09:15:13 +0100

    Implementato il file main.py

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+"""
+Realizzare in linguaggio Python una semplice applicazione che funga da traduttore di parole aliene.
+Deve essere possibile sia l’aggiunta di nuove parole che la ricerca di quelle esistenti.
+L’applicazione dovrà essere dotata di un menù stampato su terminale, tramite il quale l’utente può
+selezionare la funzionalità richiesta digitando il numero corrispondente.
+
+Le funzionalità del programma richieste sono:
+- Leggere un dizionario iniziale dal file “dictionary.txt”
+- Inserire una nuova parola e la relativa traduzione secondo il seguente pattern:
+                            <parola aliena> <traduzione> (separate da uno spazio)
+  Alla pressione del tasto invio, la parola e la sua traduzione verranno aggiunte al dizionario.
+- Cercare la traduzione di una parola esistente inserendo <parola aliena> e facendo pressione sul tasto
+invio. La traduzione verrà visualizzata sul terminale.
+
+Implementare i controlli per eventuali errori sull’input: gli unici caratteri ammessi sono [a-zA-Z] (ossia solo
+le lettere alfabetiche, siano essere maiuscole o minuscole), ma la ricerca deve essere case insensitive. Si
+suggerisce di convertire tutto il testo ricevuto in minuscolo prima di elaborarlo.
+"""
+
+
+
+
 import translator as tr
 
 t = tr.Translator()
@@ -11,15 +33,24 @@ while(True):
 
     txtIn = input()
 
-    # Add input control here!
+    acceptable_inputs = {1, 2, 3, 4, 5}
+
+    if txtIn not in acceptable_inputs:
+        raise ValueError
 
     if int(txtIn) == 1:
-        print()
+        print("Ok, quale parola devo aggiungere?")
         txtIn = input()
-        pass
+        t.handleAdd(txtIn)
     if int(txtIn) == 2:
-        pass
+        print("Ok, quale parola devo cercare?")
+        txtIn = input()
+        t.handleTranslate(txtIn)
     if int(txtIn) == 3:
-        pass
+        print("Ok, quale parola devo cercare (wildcard)?")
+        txtIn = input()
+        t.handleWildCard(txtIn)
     if int(txtIn) == 4:
+        print("Ok, termino programma.")
         break
+
--- translator.py
+++ translator.py
@@ -4,15 +4,24 @@ class Translator:
         pass
 
     def printMenu(self):
-        # 1. Aggiungi nuova parola
-        # 2. Cerca una traduzione
-        # 3. Cerca con wildcard
-        # 4. Exit
-        pass
+        print("-"*30)
+        print(" "*3+"Translator Alien-Italian"+" "*3)
+        print("-"*30)
+        print("1. Aggiungi una nuova parola")
+        print("2. Cerca una traduzione")
+        print("3. Cerca con wildcard")
+        print("4. Exit")
+        print("-" * 30)
 
     def loadDictionary(self, dict):
         # dict is a string with the filename of the dictionary
-        pass
+        words = []
+        with open(dict, "r", encoding="utf-8") as fin:
+            for riga in fin:
+                campi = riga.split()
+                tupla = (campi[0], campi[1])
+                words.append(tupla)
+            return words
 
     def handleAdd(self, entry):
         # entry is a tuple <parola_aliena> <traduzione1 traduzione2 ...>
@@ -22,6 +31,6 @@ class Translator:
         # query is a string <parola_aliena>
         pass
 
-    def handleWildCard(self,query):
+    def handleWildCard(self, query):
         # query is a string with a ? --> <par?la_aliena>
         pass
